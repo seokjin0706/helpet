be5ae213be5d3be5adecc9f0012bbfe88ca17baa
seokjin0706 <seokjin_0706@naver.com>
2022-12-21 18:26:56 +0900

    [init]

---
+++ backend/helpet/src/test/java/com/helpet/HelpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
